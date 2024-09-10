0403e317dcb086dbc5175c65e8b7a2c82ffc196e
Raoof13 <kakmogu23@gmail.com>
2024-09-10 14:04:35 +0300

    09.09.24

--- main2.py
+++ main2.py
@@ -1,5 +1,5 @@
-homework = 12
-hours = 1.5
-course = "Python"
-time_to = hours/homework
+homework=12
+hours=1.5
+course="Python"
+time_to=hours/homework
 print('Курс:',course,', всего задач:',homework,', затрачено часов:',hours,'среднее время выполнения:',time_to, )
