a2853452aa90a6326db2744ccd5ab8412eb87572
Raoof13 <kakmogu23@gmail.com>
2024-09-16 11:43:05 +0300

    09.09.24

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+my_string = "я скажу тебе то, что для тебя не новость"
+print(len(my_string))
+print(my_string.upper()) # В верхнем регистре
+print(my_string.lower()) # В нижнем регистре
+print(my_string.replace(__old=" ", __new=""))
